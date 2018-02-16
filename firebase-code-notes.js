859e68bacd0b66269251d9229ad52eb52e77b26b
ted555 <thanong984@gmail.com>
2018-02-16 14:55:26 +0700

    Method to count number of child in Firebase

--- firebase-code-notes.js
+++ firebase-code-notes.js
@@ -3,3 +3,11 @@ var sessionsRef = firebase.database().ref("sessions");
 sessionsRef.push({
   startedAt: firebase.database.ServerValue.TIMESTAMP
 });
+
+//Firebase count number of child
+firebase.database().ref('some_refer').once("value")
+  .then(function(snapshot) {
+
+    console.log(snapshot.numChildren()); // Number of child count
+    
+});
